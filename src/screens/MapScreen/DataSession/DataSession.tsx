1c6d6cdd6d0bf0360290b43d888771df86abdeaf
carlosamorim28 <81266635+carlosamorim28@users.noreply.github.com>
2026-06-12 19:24:52 -0300

    add graph

--- src/screens/MapScreen/DataSession/DataSession.tsx
+++ src/screens/MapScreen/DataSession/DataSession.tsx
@@ -1,6 +1,8 @@
 import type DataController from "../../../controllers/DataController";
 import './DataSession.css'
 import Checkbox from "../../../components/Checkbox/Checkbox";
+import Graph from "../../../components/Graph/Graph";
+
 
 export default function DataSession({dataController}: {dataController: ReturnType<typeof DataController>}): React.ReactElement {
   return(
@@ -14,7 +16,9 @@ export default function DataSession({dataController}: {dataController: ReturnTyp
       <h3 className="data-session-option-line">Ponto de máxima Obstrução:</h3>
       <span className="data-session-option-line">lat {dataController.mapController.maxInterferencePoint.lat} lng {dataController.mapController.maxInterferencePoint.lng}</span>
       <span className="data-session-option-line">Distancia: {Math.ceil(dataController.mapController.maxInterferencePointDistance * 100 / 1000) / (100)}Km</span>
-
+      <div className="graph-container" >
+        <Graph naturalPath={dataController.mapController.elevationPath} straightLine={dataController.mapController.sightLineNoObstructed} bottomFresnelElipsoid={dataController.mapController.bottomFresnelElipsoidNoObstructed} topFresnelElipsoid={dataController.mapController.topFresnelElipsoidNoObstructed} reflexiveRay={[]} maxYScale={dataController.mapController.maxScaleValue} />
+      </div>
      </div>
   )
 }
